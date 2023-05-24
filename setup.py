d2fdf432cfa0bc3382574c6233f5235110fb2c43
Mike Rabbat <mikerabbat@fb.com>
2023-05-24 08:52:43 -0700

    Package (#9)
    Summary:
    This PR addresses issue https://github.com/facebookresearch/optimizers/issues/5 making `distributed_shampoo` pip installable
    
    Pull Request resolved: https://github.com/facebookresearch/optimizers/pull/9
    
    Test Plan: pip install -e .
    
    Reviewed By: hjmshi
    
    Differential Revision: D46150980
    
    Pulled By: mikerabbat
    
    fbshipit-source-id: 69116e9bdf5ea337d8c53fc39a96fd3fb8fd6e3f

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+Copyright (c) Meta Platforms, Inc. and affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+"""
+
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="distributed_shampoo",
+        version="0.1.0",
+        description="PyTorch implementation of Distributed Shampoo",
+        license="BSD 3-clause",
+        packages=find_packages(),
+        install_requires=[],
+        python_requires=">=3.8",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        ],
+    )
