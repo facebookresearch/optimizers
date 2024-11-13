7ffbf1b6228657b3cd2e68b6011e0b27729a6f21
runame <re393@cam.ac.uk>
2024-11-13 10:14:54 -0800

    Fix type hints (#45)
    Summary: Pull Request resolved: https://github.com/facebookresearch/optimizers/pull/45
    
    Reviewed By: anana10c
    
    Differential Revision: D65882392
    
    Pulled By: tsunghsienlee
    
    fbshipit-source-id: f57a74be3c4bdaf1fdda869f30606b1f80d2a76d

--- distributed_shampoo/utils/shampoo_hsdp_distributor.py
+++ distributed_shampoo/utils/shampoo_hsdp_distributor.py
@@ -152,9 +152,9 @@ class HSDPDistributor(DistributorInterface):
 
         # Determine communication type.
         if distributed_config.communication_dtype == CommunicationDType.BF16:
-            communication_dtype: torch.dtype = torch.bfloat16
+            communication_dtype = torch.bfloat16
         elif distributed_config.communication_dtype == CommunicationDType.FP16:
-            communication_dtype: torch.dtype = torch.float16
+            communication_dtype = torch.float16
         else:
             assert distributed_config.communication_dtype in [
                 CommunicationDType.FP32,
