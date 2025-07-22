b452d1ea08ee4d8c6284f071273db1efaa626e7d
Tsung-Hsien Lee <zong@meta.com>
2025-07-22 16:37:51 -0700

    Add test for HybridShardDistributor on empty params
    Summary: This diff adds tests on how `HybridShardDistributor` reacts on empty params. This is a continuation of https://github.com/facebookresearch/optimizers/commit/3779692c4b7dc2f2d28f3337b1f1e1e1f16fd986.
    
    Reviewed By: anana10c
    
    Differential Revision: D77710215
    
    fbshipit-source-id: 2e3fbadcee39d219b620f3fc2d06f4d812103fdf

--- distributed_shampoo/utils/gpu_tests/shampoo_hybrid_shard_distributor_test.py
+++ distributed_shampoo/utils/gpu_tests/shampoo_hybrid_shard_distributor_test.py
@@ -15,7 +15,7 @@ import unittest
 from collections.abc import Callable
 from functools import partial
 from itertools import filterfalse
-from typing import overload
+from typing import cast, overload
 from unittest import mock
 
 import torch
@@ -31,9 +31,16 @@ from distributed_shampoo.tests.shampoo_test_utils import (
     construct_training_problem,
     train_model,
 )
+from distributed_shampoo.utils.gpu_tests.distributor_test_utils import (
+    DistributorOnEmptyParamTest,
+)
+from distributed_shampoo.utils.shampoo_block_info import DTensorBlockInfo
+from distributed_shampoo.utils.shampoo_hybrid_shard_distributor import (
+    HybridShardDistributor,
+)
 from distributed_shampoo.utils.shampoo_preconditioner_list import SHAMPOO
 
-from torch import nn
+from torch import distributed as dist, nn
 from torch.distributed.checkpoint._nested_dict import flatten_state_dict
 from torch.distributed.device_mesh import init_device_mesh
 from torch.distributed.fsdp import FSDPModule, fully_shard
@@ -504,3 +511,169 @@ class ShampooHybridShardDistributorTest(DTensorTestBase):
                 ),
                 model.parameters(),
             )
+
+
+@unittest.skipIf(not torch.cuda.is_available(), "Skip when CUDA is not available")
+class HybridShardDistributorOnEmptyParamTest(
+    DTensorTestBase, DistributorOnEmptyParamTest.Interface
+):
+    @property
+    def world_size(self) -> int:
+        return 4
+
+    def _construct_model_and_distributor(
+        self,
+    ) -> tuple[nn.Module, HybridShardDistributor]:
+        device_mesh = init_device_mesh(
+            "cuda", (2, 2), mesh_dim_names=("replicate", "shard")
+        )
+
+        # Create a model with specific configuration:
+        # - linear_layers are empty params (second dimension is 0)
+        # - dead_layers will be partitioned into two split params for each reploicate group
+        # For replicate group composed of rank 0 and 2:: One split param with torch.Size((PRECONDITIONER_DIM, PRECONDITIONER_DIM)) and another with torch.Size((PRECONDITIONER_DIM // 2, PRECONDITIONER_DIM))
+        # For replicate group composed of rank 1 and 3:: One split param with torch.Size((PRECONDITIONER_DIM // 2, PRECONDITIONER_DIM)) and another with torch.Size((PRECONDITIONER_DIM, PRECONDITIONER_DIM))
+        model = construct_training_problem(
+            model_linear_layers_dims=(PRECONDITIONER_DIM, 0),
+            model_dead_layers_dims=(PRECONDITIONER_DIM, 3 * PRECONDITIONER_DIM),
+            enable_learnable_scalar=False,  # Disable 0D learable parameter because FSDP doesn't support it.
+            device=torch.device("cuda"),
+            fill=0.01,
+            post_model_decoration=partial(fully_shard, mesh=device_mesh),
+        )[0]
+        distributed_config = HybridShardShampooConfig(
+            device_mesh=device_mesh, num_trainers_per_group=1
+        )
+        distributor = HybridShardDistributor(
+            param_group=DistributedShampoo(
+                model.parameters(),
+                lr=0.001,
+                betas=(0.9, 1.0),
+                epsilon=1e-8,
+                momentum=0.0,
+                weight_decay=0.0,
+                precondition_frequency=1,
+                start_preconditioning_step=-1,
+                max_preconditioner_dim=PRECONDITIONER_DIM,
+                distributed_config=distributed_config,
+            ).param_groups[0],
+            distributed_config=distributed_config,
+        )
+
+        # Get the weight of the linear layers (which is empty) and set its gradient
+        linear_layers: nn.ModuleList = cast(nn.ModuleList, model.linear_layers)
+        first_linear_layer_weight: torch.Tensor = cast(
+            torch.Tensor, linear_layers[0].weight
+        )
+        assert first_linear_layer_weight.numel() == 0
+        first_linear_layer_weight.grad = torch.ones_like(first_linear_layer_weight)
+
+        # Get the weight of the dead layers and set its gradient to None to make sure this is a dead layer
+        dead_layers: nn.ModuleList = cast(nn.ModuleList, model.dead_layers)
+        first_dead_layer_weight: torch.Tensor = cast(
+            torch.Tensor, dead_layers[0].weight
+        )
+        first_dead_layer_weight.grad = None
+
+        return model, distributor
+
+    @property
+    def _expected_masked_blocked_params(self) -> tuple[torch.Tensor, ...]:
+        return ()
+
+    @with_comms
+    def test_update_params(self) -> None:
+        DistributorOnEmptyParamTest.Interface.test_update_params(self)
+
+    @property
+    def _expected_local_grad_selector(self) -> tuple[bool, ...]:
+        return (False, False)
+
+    @with_comms
+    def test_local_grad_selector(self) -> None:
+        DistributorOnEmptyParamTest.Interface.test_local_grad_selector(self)
+
+    @property
+    def _expected_local_blocked_params(self) -> tuple[torch.Tensor, ...]:
+        # Define expected parameters for each rank
+        return (
+            torch.zeros(
+                (PRECONDITIONER_DIM, PRECONDITIONER_DIM),
+                dtype=torch.float,
+                device=f"cuda:{dist.get_rank()}",
+            ),
+            torch.zeros(
+                (PRECONDITIONER_DIM // 2, PRECONDITIONER_DIM),
+                dtype=torch.float,
+                device=f"cuda:{dist.get_rank()}",
+            ),
+        )
+
+    @with_comms
+    def test_local_blocked_params(self) -> None:
+        DistributorOnEmptyParamTest.Interface.test_local_blocked_params(self)
+
+    def _expected_local_block_info_list(
+        self, model: nn.Module
+    ) -> tuple[DTensorBlockInfo, ...]:
+        # Get the weight parameter from the first dead layer
+        dead_layers: nn.ModuleList = cast(nn.ModuleList, model.dead_layers)
+        first_dead_layer_weight: torch.Tensor = cast(
+            torch.Tensor, dead_layers[0].weight
+        )
+
+        # Define expected DTensorBlockInfo objects for each rank
+        return {
+            0: (  # For rank 0
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_0-block_0"),
+                ),
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_0-block_1"),
+                ),
+            ),
+            1: (  # For rank 1
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_1-block_0"),
+                ),
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_1-block_1"),
+                ),
+            ),
+            2: (  # For rank 2
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_0-block_0"),
+                ),
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_0-block_1"),
+                ),
+            ),
+            3: (  # For rank 3
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_1-block_0"),
+                ),
+                DTensorBlockInfo(
+                    param=first_dead_layer_weight,
+                    composable_block_ids=(0, "rank_1-block_1"),
+                ),
+            ),
+        }[dist.get_rank()]
+
+    @with_comms
+    def test_local_block_info_list(self) -> None:
+        DistributorOnEmptyParamTest.Interface.test_local_block_info_list(self)
+
+    @property
+    def _expected_local_masked_block_grads(self) -> tuple[torch.Tensor, ...]:
+        return ()
+
+    @with_comms
+    def test_merge_and_block_gradients(self) -> None:
+        DistributorOnEmptyParamTest.Interface.test_merge_and_block_gradients(self)
