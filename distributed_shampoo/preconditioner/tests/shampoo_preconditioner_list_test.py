f19287a233c8568198d66ab4bdf8622096230028
runame <re393@cam.ac.uk>
2025-08-16 14:17:09 +0200

    Add tests for SignDescentPreconditionerList

--- distributed_shampoo/preconditioner/tests/shampoo_preconditioner_list_test.py
+++ distributed_shampoo/preconditioner/tests/shampoo_preconditioner_list_test.py
@@ -36,6 +36,7 @@ from distributed_shampoo.preconditioner.shampoo_preconditioner_list import (
     PreconditionerList,
     RootInvShampooPreconditionerList,
     SGDPreconditionerList,
+    SignDescentPreconditionerList,
     SpectralDescentPreconditionerList,
 )
 from distributed_shampoo.shampoo_types import (
@@ -48,6 +49,7 @@ from distributed_shampoo.shampoo_types import (
     PreconditionerValueError,
     RootInvShampooPreconditionerConfig,
     ShampooPreconditionerConfig,
+    SignDescentPreconditionerConfig,
     SpectralDescentPreconditionerConfig,
 )
 
@@ -217,6 +219,73 @@ class SGDPreconditionerListTest(AbstractPreconditionerListTest.Interface):
             preconditioner_list=self._instantiate_preconditioner_list(),
             masked_grad_lists=[masked_grad_list],
             masked_expected_preconditioned_grad_list=masked_grad_list,
+        )
+
+    @property
+    def _expected_numel_list(self) -> tuple[int, ...]:
+        return (0, 0)
+
+    @property
+    def _expected_dims_list(self) -> tuple[torch.Size, ...]:
+        return (torch.Size([2]), torch.Size([2, 2]))
+
+    @property
+    def _expected_num_bytes_list(self) -> tuple[int, ...]:
+        return (0, 0)
+
+    @property
+    def _expected_numel(self) -> int:
+        return 0
+
+    @property
+    def _expected_num_bytes(self) -> int:
+        return 0
+
+    @property
+    def _expected_compress_list_call_count(self) -> int:
+        return 0
+
+
+@instantiate_parametrized_tests
+class SignDescentPreconditionerListTest(AbstractPreconditionerListTest.Interface):
+    def _instantiate_block_list(self) -> tuple[Tensor, ...]:
+        return (
+            torch.tensor([1.0, 2.0]),
+            torch.tensor([[3.0, 4.0], [5.0, 6.0]]),
+        )
+
+    def _instantiate_preconditioner_list(
+        self, **kwargs: Any
+    ) -> SignDescentPreconditionerList:
+        return SignDescentPreconditionerList(
+            block_list=self._block_list,
+            preconditioner_config=SignDescentPreconditionerConfig(**kwargs),
+        )
+
+    @parametrize(
+        "scale_fn",
+        (
+            lambda grad: 2.0,
+            lambda grad: grad.abs().sum(),
+        ),
+    )
+    def test_update_preconditioners_and_precondition(
+        self, scale_fn: Callable[[Tensor], float | Tensor]
+    ) -> None:
+        masked_grad_list = (
+            torch.tensor([1.0, -2.0]),
+            torch.tensor([[3.0, -4.0], [-5.0, 6.0]]),
+        )
+        masked_expected_preconditioned_grad_list = (
+            scale_fn(masked_grad_list[0]) * torch.tensor([1.0, -1.0]),
+            scale_fn(masked_grad_list[1]) * torch.tensor([[1.0, -1.0], [-1.0, 1.0]]),
+        )
+        self._verify_preconditioner_updates(
+            preconditioner_list=self._instantiate_preconditioner_list(
+                scale_fn=scale_fn
+            ),
+            masked_grad_lists=[masked_grad_list],
+            masked_expected_preconditioned_grad_list=masked_expected_preconditioned_grad_list,
         )
 
     @property
@@ -729,12 +798,15 @@ class AbstractTest:
                 )
 
         def test_amortized_computation_internal_failure(self) -> None:
-            with mock.patch.object(
-                shampoo_preconditioner_list,
-                self._amortized_computation_properties.amortized_computation_function_name,
-                # Simulate the situation throws an exception (not nan and inf) to test the warning
-                side_effect=ZeroDivisionError,
-            ) as mock_amortized_computation, self.assertLogs(level="WARNING") as cm:
+            with (
+                mock.patch.object(
+                    shampoo_preconditioner_list,
+                    self._amortized_computation_properties.amortized_computation_function_name,
+                    # Simulate the situation throws an exception (not nan and inf) to test the warning
+                    side_effect=ZeroDivisionError,
+                ) as mock_amortized_computation,
+                self.assertLogs(level="WARNING") as cm,
+            ):
                 self._preconditioner_list.update_preconditioners(
                     masked_grad_list=(
                         torch.tensor([1.0, 0.0]),
@@ -788,25 +860,27 @@ class AbstractTest:
             )
             all_fail = (fail,) * NUM_AMORTIZED_COMPUTATION_CALLS
             all_success = (success,) * NUM_AMORTIZED_COMPUTATION_CALLS
-            with mock.patch.object(
-                shampoo_preconditioner_list,
-                self._amortized_computation_properties.amortized_computation_function_name,
-                # Note that the cases causally depend on each other.
-                side_effect=[
-                    # Case 1: amortized computation fails less often than tolerance.
-                    *all_but_one_fail,  # Success for a single Kronecker factor is not enough to reset counter.
-                    # Case 2: amortized computation fails exactly as often as tolerance (3).
-                    *all_fail,
-                    *all_fail,
-                    # Case 3: amortized computation succeeds after tolerance hit (counter is reset).
-                    *all_success,
-                    # Case 4: amortized computation fails more often than tolerance.
-                    *all_fail,
-                    *all_fail,
-                    *all_fail,
-                    fail,  # One failure is enough to raise an exception in this case.
-                ],
-            ) as mock_amortized_computation:
+            with (
+                mock.patch.object(
+                    shampoo_preconditioner_list,
+                    self._amortized_computation_properties.amortized_computation_function_name,
+                    # Note that the cases causally depend on each other.
+                    side_effect=[
+                        # Case 1: amortized computation fails less often than tolerance.
+                        *all_but_one_fail,  # Success for a single Kronecker factor is not enough to reset counter.
+                        # Case 2: amortized computation fails exactly as often as tolerance (3).
+                        *all_fail,
+                        *all_fail,
+                        # Case 3: amortized computation succeeds after tolerance hit (counter is reset).
+                        *all_success,
+                        # Case 4: amortized computation fails more often than tolerance.
+                        *all_fail,
+                        *all_fail,
+                        *all_fail,
+                        fail,  # One failure is enough to raise an exception in this case.
+                    ],
+                ) as mock_amortized_computation
+            ):
                 # Accumulate factor matrices for valid amortized computation.
                 self._preconditioner_list.update_preconditioners(
                     masked_grad_list=masked_grad_list0,
