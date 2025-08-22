Assert the gradients are always non NaN/Inf to prevent incorrect computation
Summary: There are similar Nan/Inf checks performed at the updates of factor matrices and inverse factor matrices; however, those checks won't get triggered until `start_preconditioning_step`, this might hide problems for a long time when users setting `start_preconditioning_step` to a value pretty large. As a result, adding this checks to alarm this as early as possible.

Reviewed By: wz337

Differential Revision: D80676166

fbshipit-source-id: 2fd00af72f67b9ce9ad6cd5123bc2f5a4c030923

distributed_shampoo/distributor/shampoo_distributor.py
@@ -267,7 +267,10 @@ class DistributorInterface(ABC):
                 # Skip multi_dim_split if this blocked grad will not be used locally.
                 continue
 
-            assert grad is not None
+            assert (
+                grad is not None and torch.isfinite(grad).all()
+            ), f"Encountered gradient containing NaN/Inf in parameter with shape {attrgetter('shape')(grad)}. Check your model for numerical instability or consider gradient clipping."
+
             # Obtain blocks for each gradient after merging.
             blocks_within_grad = multi_dim_split(
                 grad.view(merge_dims(tensor_shape=grad.size())),